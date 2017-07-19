575d76e30cbac1c867b0e9072892da657b752790
Daniel Genis <daniel@byte.nl>
2017-07-19 13:58:36 +0200

    post-install set ncr to be started on boot

--- packaging/after-install.sh
+++ packaging/after-install.sh
@@ -6,6 +6,7 @@ useradd --system ncr --home /opt/ncr --groups nagios
 if [ -x /bin/systemctl ]; then
 	/bin/systemctl daemon-reload
 	/bin/systemctl start ncr.service
+	/bin/systemctl enable ncr.service
 elif [ -x /usr/sbin/service ]; then
 	/sbin/initctl reload-configuration
 	/usr/sbin/service ncr start
